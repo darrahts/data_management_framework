c5195395aed3a7d0f1b9ffb87856f200a0ec6754
Timothy Darrah <31658834+darrahts@users.noreply.github.com>
2022-06-30 13:16:28 +0200

    Create create_functions.sql

---
+++ sql/create_functions.sql
@@ -0,0 +1,43 @@
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+/*
+	  This script install functions in the database
+*/
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+
+
+/*
+    This is the function requred to generate an empty component table every time an asset type 
+    is created. 
+*/
+create or replace function generate_table()
+  returns trigger as 
+   $$ 
+    begin
+      execute format('create table "%s_%s_tb"(id serial primary key not null);', new."type", new."subtype");
+    return new;
+  end;
+  $$
+  language 'plpgsql';
+
+/*
+    This generates the trigger for the above function.
+*/
+create trigger generate_table_trigger
+	after insert 
+	on asset_type_tb
+	for each row 
+	execute procedure generate_table();
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
